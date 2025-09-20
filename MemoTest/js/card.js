49cb1e99a64aee0519ad607fe28bef7e8090b3a0
LucasFrey-tech <lucasfrey19@gmail.com>
2025-09-20 16:50:06 -0300

    card changes

--- MemoTest/js/card.js
+++ MemoTest/js/card.js
@@ -1,30 +1,35 @@
-class carta {
-    constructor(id, imagen) {
-        this.id = id;
-        this.imagen = imagen;
-        this.volteada = false;
-
-        this.elemento = this.crearElemento();
+export class Carta {
+    constructor(imagen) {
+        this.face = imagen;
+        this.back = '../resources/cardIcons/honeycomb-seamless-pattern-hexagon-mosaic-260nw-2529836897.png'
+        this.flipped = false;
+        // Al momento de crear el objeto este se crea el elemento que lo contiene
+        this.card = this.crearElemento();
     }
 
+    // Funcion para crear el elemento carta
     crearElemento() {
         const div = document.createElement("div");
         div.classList.add("carta");
-        div.dataset.id = this.id;
-
+        div.dataset = this.id;
+    
         const img = document.createElement("img");
-        img.src = this.imagen;
+        img.src = this.face;
         img.style.display = "none";
 
+        // Se agrega al elemento div la imagen correspondiente
         div.appendChild(img);
-
+        // Se espera por un click para voltear la carta
         div.addEventListener("click", () => this.voltear())
 
         return div;
     }
 
+    // Funcion para voltear la carta 
     voltear() {
+        // Se voltea la carta hacia un lado u otro
         this.volteada = !this.volteada;
+        // Se selecciona la imagen
         const img = this.elemento.querySelector("img");
         img.style.display = this.volteada ? "block" : "none";
     }
