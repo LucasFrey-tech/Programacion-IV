614579b071f61fbc83b86e37ae7f0c7ad214a02c
LucasFrey-tech <lucasfrey19@gmail.com>
2025-09-22 01:15:51 -0300

    Fix(Flip): Renombre de funcion + correcion y agregar html class

--- MemoTest/js/card.js
+++ MemoTest/js/card.js
@@ -1,36 +1,31 @@
 export class Carta {
     constructor(imagen) {
         this.face = imagen;
-        this.back = '../resources/cardIcons/honeycomb-seamless-pattern-hexagon-mosaic-260nw-2529836897.png'
+        this.back = "resources/cardIcons/honeycomb-seamless-pattern-hexagon-mosaic-260nw-2529836897.png";
         this.flipped = false;
-        // Al momento de crear el objeto este se crea el elemento que lo contiene
-        this.card = this.crearElemento();
+        this.element = null;
+        this.img = null;
     }
 
-    // Funcion para crear el elemento carta
-    crearElemento() {
-        const div = document.createElement("div");
-        div.classList.add("carta");
-        div.dataset = this.id;
     
-        const img = document.createElement("img");
-        img.src = this.face;
-        img.style.display = "none";
-
-        // Se agrega al elemento div la imagen correspondiente
-        div.appendChild(img);
-        // Se espera por un click para voltear la carta
-        div.addEventListener("click", () => this.voltear())
-
-        return div;
+    flip() {
+        this.flipped = !this.flipped;
+        this.element.classList.toggle("flipped", this.flipped);
+        this.img.src = this.flipped ? this.face : this.back;
     }
 
-    // Funcion para voltear la carta 
-    voltear() {
-        // Se voltea la carta hacia un lado u otro
-        this.volteada = !this.volteada;
-        // Se selecciona la imagen
-        const img = this.elemento.querySelector("img");
-        img.style.display = this.volteada ? "block" : "none";
+    createElement() {
+        const card = document.createElement("div");
+        card.classList.add("Card");
+
+        const img = document.createElement("img");
+        img.src = this.back;
+
+        card.appendChild(img);
+
+        this.element = card;
+        this.img = img;
+
+        return card;
     }
 }
