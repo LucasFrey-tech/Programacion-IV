a4e39b953e28a49eecbddc0958407f7a35b85bb6
LucasFrey-tech <lucasfrey19@gmail.com>
2025-09-22 13:15:45 -0300

    feat(Panel): agregar clase Panel

---
+++ MemoTest/js/panel.js
@@ -0,0 +1,27 @@
+export class Panel {
+    constructor() {
+        this.foundPairs = 0;
+        this.movements = 0;
+        this.docPairs = document.getElementById("Pairs");
+        this.docMovements = document.getElementById("Attempts");
+    }
+
+    founded() {
+        this.foundPairs++;
+        this.docPairs.textContent = this.foundPairs;
+    }
+
+    attempts() {
+        this.movements++;
+        this.docMovements.textContent = this.movements;
+    }
+
+    setFoundPairs(val) {
+        this.docPairs.textContent = val;
+    }
+
+    setMovements(val) {
+        this.docMovements.textContent = val;
+    }
+
+}
